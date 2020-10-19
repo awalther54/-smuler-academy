50ae243de669f66ef5bf2636d1c63f72410aed83
Anthony Walther <31695310+awalther54@users.noreply.github.com>
2020-10-20 00:41:57 +0200

    Initialisation du projet

---
+++ challenges.php
@@ -0,0 +1,38 @@
+<?php
+$challenges = [
+    "Mai 2020" => [
+        "Joie et tristesse",
+        "Fantastique imaginaire",
+    ],
+    "Juillet 2020" => [
+        "Comédie Musicale et Disney",
+        "Ange et démon",
+        "Voyage, voyage",
+        "Duos forcés",
+        "L'amour",
+    ],
+    "Juin 2020" => [
+        "Top Chrono",
+        "Sosie et imitation",
+        "A capella",
+        "Gospel",
+    ],
+    "Août 2020" => [
+        "Dessins animés",
+        "Animaux",
+        "Dans le noir",
+        "Année de naissance",
+        "Habillez-vous!",
+    ],
+    "Septembre 2020" => [
+        "The Voice #1",
+        "Génération Boys Band",
+        "Céline Dion Incognito",
+        "Surprise !",
+    ],
+    "Octobre 2020" => [
+        "Vous avez dit vulgaire...",
+    ],
+];
+include('challenges.html');
+?>
---
+++ index.php
@@ -0,0 +1,48 @@
+<?php
+$challenges = [
+    "Joie et tristesse",
+    "Fantastique et Imagination",
+    "Comédie musicale et disney",
+    "Ange et démon",
+    "Voyage, voyage",
+    "Duos forcés",
+    "L'amour",
+    "Top Chrono",
+    "Sosie et imitations",
+    "A capella",
+    "Gospel",
+    "Dessins animés",
+    "Animaux",
+    "Dans le noir",
+    "Année de naissance",
+    "Habillez-vous",
+    "The Voice #1",
+    "Génération boys band",
+    "Céline Dion incognito",
+    "Surprise !",
+    "Vous avez dit vulgaire...",
+];
+$defis = [
+    "Peluche",
+    "Brosse à dent",
+    "Aveugle",
+    "Edith Piaf",
+    "Casserole",
+    "Crayon",
+    "Goinfre",
+    "Chimpmunk",
+    "Oxygène",
+    "Chèvre",
+    "Alcool",
+    "Hydratation",
+    "Langue",
+    "Ressort",
+    "Impro",
+    "Genre",
+    "Chips",
+    "Compil des Jean-Michel",
+];
+sort($challenges);
+sort($defis);
+include('index.html');
+?>
